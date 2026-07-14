3cb6af09cae5ab469afd2e65498e85706522cba5
nawin24 <nawinbg24@gmail.com>
2026-07-14 16:51:07 +0530

    Update for Firebase deployment

--- next.config.js
+++ next.config.js
@@ -2,10 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
-  // Add this for serialport support
+  
+  output: 'standalone',
+  swcMinify: true,
+  
   experimental: {
     serverComponentsExternalPackages: ['serialport', '@serialport/parser-readline'],
+    // optimizeCss: true, // ← COMMENT THIS OUT or remove it
   },
+  
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.externals = [...config.externals, 'serialport', '@serialport/parser-readline'];
@@ -23,9 +28,39 @@ module.exports = nextConfig;
 
 
 
+
+
+
+
+
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
 //   reactStrictMode: true,
 //   images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
+//   // Add this for serialport support
+//   experimental: {
+//     serverComponentsExternalPackages: ['serialport', '@serialport/parser-readline'],
+//   },
+//   webpack: (config, { isServer }) => {
+//     if (isServer) {
+//       config.externals = [...config.externals, 'serialport', '@serialport/parser-readline'];
+//     }
+//     return config;
+//   },
 // };
 // module.exports = nextConfig;
+
+
+
+
+
+
+
+
+
+// // /** @type {import('next').NextConfig} */
+// // const nextConfig = {
+// //   reactStrictMode: true,
+// //   images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
+// // };
+// // module.exports = nextConfig;
