b05f5d576879e496487764fd69eb93b1b078fa5b
nawin24 <nawinbg24@gmail.com>
2026-07-16 15:54:30 +0530

    Update image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
+  images: { remotePatterns: [{ protocol: "https", hostname: "**" }] ,
+ unoptimized: true, 
+},
+  
   // Add this for serialport support
   experimental: {
     serverComponentsExternalPackages: ['serialport', '@serialport/parser-readline'],
